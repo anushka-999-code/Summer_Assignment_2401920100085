b55021902a61c822722485fa103bbe12ec611ab9
anushka <anushkatongar108@gmail.com>
2026-06-01 21:50:57 +0530

    added week 1 day 1 codes

---
+++ Week_1/Day_1/Remove_duplicate_from_sorted_array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+            for(int k=1;k<nums.size();k++){
+                if(nums[k]!=nums[i]){
+                    i++;
+                    nums[i]=nums[k];
+                }
+            }
+            return i+1;
+    }
+};
