88961ad92fe65be78f1fb6036424e826f623dfee
anushka <anushkatongar108@gmail.com>
2026-06-04 22:05:54 +0530

    added week 1 day 4 code

---
+++ Week_1/Day_4/Matrix_diagonal_sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        int diag_sum=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==j||i+j==n-1){
+                    diag_sum+=mat[i][j];
+                }
+            }
+        }
+        return diag_sum;
+    }
+};
