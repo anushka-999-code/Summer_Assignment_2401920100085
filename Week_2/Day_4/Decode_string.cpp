8530b8d06399009881b5b01cb3e48fc9cd6272ae
anushka <anushkatongar108@gmail.com>
2026-06-12 10:32:09 +0530

    added week 2 day4 codes

---
+++ Week_2/Day_4/Decode_string.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string decodeString(string s) {
+      stack<int> counts;
+        stack<string> strs;
+
+        string curr = "";
+        int num = 0;
+
+        for (char c : s) {
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+            else if (c == '[') {
+                counts.push(num);
+                strs.push(curr);
+
+                num = 0;
+                curr = "";
+            }
+            else if (c == ']') {
+                int k = counts.top();
+                counts.pop();
+
+                string prev = strs.top();
+                strs.pop();
+
+                while (k--) {
+                    prev += curr;
+                }
+
+                curr = prev;
+            }
+            else {
+                curr += c;
+            }
+        }
+
+        return curr;  
+    }
+};
