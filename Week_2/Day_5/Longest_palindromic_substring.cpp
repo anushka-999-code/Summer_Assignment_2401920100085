5bcf5ce7edb7b7cd0ece65206f66400fbecc6c99
anushka <anushkatongar108@gmail.com>
2026-06-12 10:45:12 +0530

    added week 2 day 5 codes

---
+++ Week_2/Day_5/Longest_palindromic_substring.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int start = 0, maxLen = 1;
+        
+        for (int i = 0; i < s.size(); i++) {
+            expand(s, i, i, start, maxLen);       // odd length
+            expand(s, i, i + 1, start, maxLen);   // even length
+        }
+        
+        return s.substr(start, maxLen);
+    }
+
+private:
+    void expand(string& s, int left, int right, int& start, int& maxLen) {
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            if (right - left + 1 > maxLen) {
+                maxLen = right - left + 1;
+                start = left;
+            }
+            left--;
+            right++;
+        }
+    }
+};
